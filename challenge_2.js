c19cb0094e8501510b4dc2f568401fe56a5b7472
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-01-15 21:32:57 -1000

    completes challenge

--- challenge_2.js
+++ challenge_2.js
@@ -1,6 +1,29 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
-  var sum = 0;
   // do your work here
+  var oneFibBefore = 1;
+  //variable to store value fibonacci number before current 
+  var twoFibBefore = 0;
+  //variable to store fibonacci number before oneFibBefore
+  var current = 1;
+  //starting point
+  var sum = 0;
+
+  while(current <= n){
+    //while current is less than or equal to max value
+    if(current === oneFibBefore + twoFibBefore){
+      //if current number is equal to the sum of oneFibBefore and twoFibBefore AND is also even
+      if(current % 2 === 0) {
+      sum += current;
+      //add to sum if even
+    } 
+      twoFibBefore = oneFibBefore;
+      //moves twoFibBefore to to placement of oneFibBefore
+      oneFibBefore = current;
+      //moves oneFibBefore to placemnt of current value
+    }
+    current++;
+    //increments current by one so we can check all numbers, includining non-fibonacci numbers.
+  }
   
   return sum;
 };
